d9d274676f58236ef40f97e915fd027d2d6dbc14
BM0500 <bm0500@126.com>
2019-05-10 07:17:13 +0800

    第一次提交

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -9,9 +9,9 @@ mnist = input_data.read_data_sets('mnist_data',one_hot=True)
 one_hot 独热码
 0 1 2 3 4 5 6 7 8 9
 0 1000000000
-1 0100000000
-...
+1 0100000000...
 '''
+
 #/255归一化，把灰度区间放到01之间，方便优化器找到其中最小值
 input_x = tf.placeholder(tf.float32,[None,28*28]) / 255  #输入
 output_y = tf.placeholder(tf.float32,[None,10]) #输出十个数字的标签
