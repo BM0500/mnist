248d80388cc203d13e1b9f9146f50bb51ba039c3
孙浩钧 <38155826+BM0500@users.noreply.github.com>
2019-05-09 19:46:57 +0800

    Delete e.py

--- e.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-#下载并安装mnist手写数字识别库（55000*28*28）
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets('mnist_data',one_hot=True)
